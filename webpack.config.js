28f99c74088bcde80446edf469db7621a6b0500d
Pipeman <tizianodg@gmail.com>
2018-07-29 19:40:01 +0100

    3 (chore): Add dependencies to build a React app
    Add babel dependencies, html-loader and html-webpack-plugin.
    Add configuration options to webpack config file in order to build
    properly a react app making use of the new dependencies.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,39 @@
 const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use:
+        [
+            {
+            loader: "babel-loader"
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: false,
     port: 9000
-  }
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
 };
